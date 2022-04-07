501d60630a280f45c1c7070d1f4d588aa8250e0d
Ching Yi, Chan <qrtt1@infuseai.io>
2022-04-07 10:28:48 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- piperider_cli/workspace.py
+++ piperider_cli/workspace.py
@@ -8,3 +8,7 @@ def init():
     if not os.path.exists(working_dir):
         for s in sub_dirs:
             os.makedirs(os.path.join(working_dir, s), exist_ok=True)
+
+    # 1. init to create an example to use local file (a csv) by qrtt1
+    # 2. sources/ local file convert to ge datasource
+    # 3. stages/ read config and validate source and test structure (invoke with `run`)
