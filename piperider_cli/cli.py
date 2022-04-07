d6f29f72cd505677fafe0eb9399234fd40b3511c
Ching Yi, Chan <qrtt1@infuseai.io>
2022-04-07 14:53:00 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- piperider_cli/cli.py
+++ piperider_cli/cli.py
@@ -11,6 +11,7 @@ def cli():
 @cli.command()
 def init():
     # TODO show the process and message to users
+    click.echo('Initialize piperider')
     workspace.init()
 
 
@@ -18,4 +19,5 @@ def init():
 def run():
     # TODO check the args are "stages" files
     # invoke the stage -> piperider_cli.data.execute_great_expectation
+    # generate the report file or directory
     pass
