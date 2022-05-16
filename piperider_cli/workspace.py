91193cd091d08e63c5d120a18eeccc15cba96bb3
Kent Huang <kentwelcome@gmail.com>
2022-05-16 16:26:56 +0800

    [Feature] sc-25903 add lots of TODO for init command
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>
    Co-authored-by: Ching-Yi Chan  <qrtt1@infuseai.io>

--- piperider_cli/workspace.py
+++ piperider_cli/workspace.py
@@ -4,8 +4,41 @@ import shutil
 from ruamel.yaml import YAML
 
 yaml = YAML(typ="safe")
-
 PIPERIDER_WORKSPACE_NAME = '.piperider'
+
+
+class Configuration(object):
+    """
+    Configuration represents the config file in the piperider project
+    at $PROJECT_ROOT./piperider/config.yml
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_dbt_profile(cls, dbt_profile_path):
+        """
+        build configuration from the existing dbt profile
+
+        :param dbt_profile_path:
+        :return:
+        """
+        # TODO create configuration from dbt profile
+        raise NotImplemented
+
+    @classmethod
+    def load(cls):
+        """
+        load from the existing configuration
+
+        :return:
+        """
+        raise NotImplemented
+
+    def to_sqlalchemy_config(self, datasource_name):
+        # TODO we will convert a data source to a sqlalchemy parameters
+        raise NotImplemented
 
 
 def _generate_piperider_workspace():
@@ -15,30 +48,44 @@ def _generate_piperider_workspace():
     shutil.copytree(init_template_dir, working_dir, dirs_exist_ok=True)
 
 
+def _ask_user_for_datasource():
+    # TODO: ask the user for datasource
+    # we only support snowfalke and pg only
+    # we might consider a sqlite for dev mode?
+    raise NotImplemented
+    # return Configuration()
+
+
 def _generate_configuration(dbt_project_path=None):
+    """
+    :param dbt_project_path:
+    :return: Configuration object
+    """
     if dbt_project_path is None:
-        print('TBD: generate configuration without dbt project')
-        return
-    else:
-        dbt_profile_path = os.path.join(os.path.expanduser('~'), '.dbt/profiles.yml')
-        profile_name, target_name, type_name = _dbt_project_parser(dbt_project_path, dbt_profile_path)
-        config = dict({
-            'dataSources': [
-                {
-                    'name': profile_name,
-                    'type': type_name,
-                    'dbt': {
-                        'project': profile_name,
-                        'target': target_name,
-                        'profile': dbt_profile_path
-                    }
+        return _ask_user_for_datasource();
+
+    # TODO move datssource extracting to the Configuration.from_dbt_profile
+    dbt_profile_path = os.path.join(os.path.expanduser('~'), '.dbt/profiles.yml')
+    profile_name, target_name, type_name = _dbt_project_parser(dbt_project_path, dbt_profile_path)
+    config = dict({
+        'dataSources': [
+            {
+                'name': profile_name,
+                'type': type_name,
+                'dbt': {
+                    'project': profile_name,
+                    'target': target_name,
+                    'profile': dbt_profile_path
                 }
-            ]
-        })
-        piperider_config_path = os.path.join(os.getcwd(), PIPERIDER_WORKSPACE_NAME, 'config.yml')
-        with open(piperider_config_path, 'w') as fd:
-            yaml.dump(config, fd)
-        return
+            }
+        ]
+    })
+    piperider_config_path = os.path.join(os.getcwd(), PIPERIDER_WORKSPACE_NAME, 'config.yml')
+    with open(piperider_config_path, 'w') as fd:
+        yaml.dump(config, fd)
+
+    # TODO return config object
+    return Configuration.from_dbt_profile(dbt_profile_path)
 
 
 def _dbt_project_parser(dbt_project_path, dbt_profile_path):
@@ -62,5 +109,29 @@ def _dbt_project_parser(dbt_project_path, dbt_profile_path):
 
 
 def init(dbt_project_path=None):
-    _generate_piperider_workspace()
-    _generate_configuration(dbt_project_path=dbt_project_path)
+    try:
+        _generate_piperider_workspace()
+        # get Configuration object from dbt or user created configuation
+        configuration = _generate_configuration(dbt_project_path=dbt_project_path)
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def debug(configuration=None):
+    if not configuration:
+        configuration = Configuration.load()
+    # TODO debug with configuration
+    # test configuration format
+    # test connection working
+    raise NotImplemented
+
+
+def run():
+    configuration = Configuration.load()
+    # TODO ....
+
+
+def generateReport():
+    raise NotImplemented
