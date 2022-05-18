1f19495b31c47e350dfe41a02e69f03f90a1dde2
popcorny <celu@infuseai.io>
2022-05-18 10:48:45 +0800

    Implement the profiler
    Signed-off-by: popcorny <celu@infuseai.io>

--- piperider_cli/profiler/profiler.py
+++ piperider_cli/profiler/profiler.py
@@ -13,54 +13,7 @@ class Profiler:
         # metadata.reflect(bind=engine)
 
     def profile_column(self, table_name, column_name):
-        metadata = MetaData()
-        t = Table(table_name, metadata, Column(column_name, String))
-        # t = self.metadata.tables[table_name]
-
-        with engine.connect() as conn:
-            t2 = select(t.c[column_name].label("c")).cte(name="T")
-            stmt = select(
-                func.count().label("_total"),
-                func.count(t2.c.c).label("_non_nulls"),
-                func.count(distinct(t2.c.c)).label("_distinct"),
-                func.sum(t2.c.c).label("_sum"),
-                func.avg(t2.c.c).label("_avg"),
-                func.min(t2.c.c).label("_min"),
-                func.max(t2.c.c).label("_max"),
-                func.median(t2.c.c).label("_median"),
-            )
-            result = conn.execute(stmt).fetchone()
-            _total, _non_null, _distinct, _sum, _avg, _min, _max, _median = result
-            _total            
-            # print(f"total={_total}")
-            # print(f"non_null={_non_null}")
-            # print(f"distinct={_distinct}")
-
-            distribution = []
-            stmt = select(
-                t2.c.c,
-                func.count(t2.c.c).label("_count")
-            ).group_by(
-                t2.c.c
-            ).order_by(
-                func.count(t2.c.c).desc(),
-
-            ).limit(20)
-            result = conn.execute(stmt)            
-            for row in result:                
-                _value, _value_count = row
-                distribution += [[float(_value), _value_count]]
-            return {
-                'total': _total,
-                'non_nulls': _non_null,
-                'distinct': _distinct,
-                'min': float(_min),
-                'max': float(_max),
-                'sum': float(_sum),
-                'avg': float(_avg),
-                'median': float(_median),
-                'distribution': distribution,
-            }
+        return self.profile_numeric_column(table_name, column_name)
 
     def profile_string_column(self, table_name, column_name):
         metadata = MetaData()
@@ -76,19 +29,6 @@ class Profiler:
             )
             result = conn.execute(stmt).fetchone()
             _total, _non_null, _distinct = result
-            # print(f"total={_total}")
-            # print(f"non_null={_non_null}")
-            # print(f"distinct={_distinct}")
-
-            # with T as (
-            # select symbol as c from price
-            # )
-            # select top 20
-            #     c,
-            #     count(c) as _count
-            # from T
-            # group by c
-            # order by _count desc
             stmt = select(
                 t2.c.c,
                 func.count(t2.c.c).label("_count")
@@ -109,6 +49,68 @@ class Profiler:
                 'distribution': distribution,
             }
 
+    def profile_numeric_column(self, table_name, column_name):
+        metadata = MetaData()
+        t = Table(table_name, metadata, Column(column_name, String))
+        # t = self.metadata.tables[table_name]
+
+        with engine.connect() as conn:
+            t2 = select(t.c[column_name].label("c")).cte(name="T")
+            stmt = select(
+                func.count().label("_total"),
+                func.count(t2.c.c).label("_non_nulls"),
+                func.count(distinct(t2.c.c)).label("_distinct"),
+                func.sum(t2.c.c).label("_sum"),
+                func.avg(t2.c.c).label("_avg"),
+                func.min(t2.c.c).label("_min"),
+                func.max(t2.c.c).label("_max"),
+                func.median(t2.c.c).label("_median"),
+            )
+            result = conn.execute(stmt).fetchone()
+            _total, _non_null, _distinct, _sum, _avg, _min, _max, _median = result
+            _sum = float(_sum)
+            _avg = float(_avg)
+            _min = float(_min)
+            _max = float(_max)
+            _median = float(_median)
+
+            def width_bucket(expr, min_value, max_value, num_buckets):
+                interval = (max_value - min_value) / num_buckets
+                cases = []
+                for i in range(num_buckets):
+                    bound = min_value + interval * i
+                    cases += [(expr < bound, i)]
+                cases += [(expr <= max_value, num_buckets)]
+                return case(
+                    cases, else_=num_buckets+1
+                )
+
+            t2 = select(t.c[column_name].label("c"), width_bucket(t.c[column_name].label("c"), _min, _max, 20).label("bucket")).cte(name="T")
+            distribution = []
+            stmt = select(
+                t2.c.bucket,
+                func.count(t2.c.bucket).label("_count")
+            ).group_by(
+                t2.c.bucket
+            ).order_by(
+                t2.c.bucket
+            )
+            print(stmt)
+            result = conn.execute(stmt)
+            for row in result:
+                _bucket, _value_count = row
+                distribution += [[_bucket, _value_count]]
+            return {
+                'total': _total,
+                'non_nulls': _non_null,
+                'distinct': _distinct,
+                'min': float(_min),
+                'max': float(_max),
+                'sum': float(_sum),
+                'avg': float(_avg),
+                'median': float(_median),
+                'distribution': distribution,
+            }
 
 if __name__ == '__main__':
     user= os.getenv("SNOWFLAKE_USER")
