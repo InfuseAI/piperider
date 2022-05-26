22571a47feaa3ab5850bd0f7ef728dba1c8b629c
Ching Yi, Chan <qrtt1@infuseai.io>
2022-05-26 10:48:53 +0800

    compatible with python version less than 3.9
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- tests/profiler/test_profiler.py
+++ tests/profiler/test_profiler.py
@@ -1,11 +1,12 @@
 from piperider_cli.profiler import Profiler
 from sqlalchemy import *
+from typing import List
 
 
 class TestProfiler:
     engine = None
 
-    def create_table(self, table_name: str, data: list[tuple], columns=None, metadata=None):
+    def create_table(self, table_name: str, data: List[tuple], columns=None, metadata=None):
         header = data[0]
         data = data[1:]
 
@@ -13,18 +14,18 @@ class TestProfiler:
             metadata = MetaData()
 
         if not columns:
-            columns=[]
+            columns = []
             if len(data) == 0:
-                raise Exception("columns is not specified and data is empty")            
-            first=data[0]
+                raise Exception("columns is not specified and data is empty")
+            first = data[0]
             for i in range(len(header)):
-                col_name=header[i]
+                col_name = header[i]
                 value = first[i]
                 col = None
                 if isinstance(value, str):
-                    col=Column(col_name, String)
+                    col = Column(col_name, String)
                 elif isinstance(value, int):
-                    col=Column(col_name, Integer)                
+                    col = Column(col_name, Integer)
                 else:
                     raise Exception(f"not support type: {type(value)}")
                 columns.append(col)
@@ -36,19 +37,19 @@ class TestProfiler:
                 row_data = dict(zip(header, row))
                 stmt = (
                     insert(table).
-                    values(**row_data)
+                        values(**row_data)
                 )
                 conn.execute(stmt)
-            
+
         return table
 
     def test_basic_profile(self):
-        engine = self.engine = create_engine('sqlite://')        
+        engine = self.engine = create_engine('sqlite://')
 
         data = [
-            ("user_id", "user_name","age"),
+            ("user_id", "user_name", "age"),
             (1, "bob", 23),
-            (2, "alice",25),
+            (2, "alice", 25),
         ]
         self.create_table("test1", data)
         self.create_table("test2", data)
@@ -60,10 +61,10 @@ class TestProfiler:
         profiler = Profiler(engine)
         result = profiler.profile(tables=["test1"])
         assert "test1" in result["tables"]
-        assert "test2" not in result["tables"] 
+        assert "test2" not in result["tables"]
 
     def test_numeric_boundary(self):
-        engine = self.engine = create_engine('sqlite://')        
+        engine = self.engine = create_engine('sqlite://')
 
         data = [
             ("num",),
@@ -78,7 +79,7 @@ class TestProfiler:
         assert result["tables"]["test"]['columns']["num"]["distribution"]["counts"][5] == 0
 
     def test_empty_table(self):
-        engine = self.engine = create_engine('sqlite://')        
+        engine = self.engine = create_engine('sqlite://')
 
         data = [
             ("num", "str"),
@@ -90,14 +91,14 @@ class TestProfiler:
         assert result["tables"]["test"]['columns']["str"]["distribution"] == None
 
     def test_one_row_table(self):
-        engine = self.engine = create_engine('sqlite://')        
+        engine = self.engine = create_engine('sqlite://')
 
         data = [
             ("num", "str", "num_empty"),
             (1, "hello", None),
         ]
         self.create_table("test", data, columns=[
-            Column("num", Integer), 
+            Column("num", Integer),
             Column("str", String),
             Column("num_empty", Integer)
         ])
@@ -107,16 +108,15 @@ class TestProfiler:
         assert result["tables"]["test"]['columns']["str"]["distribution"]["counts"][0] == 1
         assert result["tables"]["test"]['columns']["num_empty"]["distribution"] == None
 
-        
     def test_range(self):
-        tests=[
+        tests = [
             (0.1, 200, 0, 10),
-            (150, 250, 150, 5),    
+            (150, 250, 150, 5),
             (8700, 15000, 8500, 500),
             (235, 753, 0, 50),
-            (20, 250, 0, 20),        
-            (50, 700, 0, 50),            
-            (5000, 70000, 0, 5000), 
+            (20, 250, 0, 20),
+            (50, 700, 0, 50),
+            (5000, 70000, 0, 5000),
             (0.05, 0.0788, 0.05, 0.002),
             (1, 21, 0, 2),
             (-1, 100, -10, 10),
@@ -129,8 +129,7 @@ class TestProfiler:
         ]
 
         for test in tests:
-            low,high, emin, einterval =test
-            min, max, interval = Profiler._calc_numeric_range(low,high)
-            assert emin==min
-            assert einterval==interval
-
+            low, high, emin, einterval = test
+            min, max, interval = Profiler._calc_numeric_range(low, high)
+            assert emin == min
+            assert einterval == interval
