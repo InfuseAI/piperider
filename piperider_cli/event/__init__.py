9434f03ddffd644aa7caa84cd7a826ea7f12b052
Timothy Lee <ctiml@infuseai.io>
2022-06-08 18:04:32 +0800

    Add a flag to enable anonymous tracking
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- piperider_cli/event/__init__.py
+++ piperider_cli/event/__init__.py
@@ -8,28 +8,26 @@ PIPERIDER_USER_HOME = os.path.expanduser('~/.piperider')
 PIPERIDER_USER_PROFILE = os.path.join(PIPERIDER_USER_HOME, 'profile.yml')
 
 _collector = Collector()
-_yml = yaml.YAML(typ='safe')
+_yml = yaml.YAML()
 
 
 def init():
     api_key = os.environ.get('PIPERIDER_EVENT_API_KEY', None)
-    user_id = None
+    user_profile = None
 
     if not os.path.exists(PIPERIDER_USER_PROFILE):
-        user_id = _generate_user_id()
+        user_profile = _generate_user_profile()
     else:
         with open(PIPERIDER_USER_PROFILE, 'r') as f:
-            profile = _yml.load(f)
-            user_id = profile.get('user_id', None)
-
-    if user_id is None:
-        user_id = _generate_user_id()
+            user_profile = _yml.load(f)
+            if user_profile.get('user_id') is None:
+                user_profile = _generate_user_profile()
 
     _collector.set_api_key(api_key)
-    _collector.set_user_id(user_id)
+    _collector.set_user_id(user_profile.get('user_id'))
 
 
-def _generate_user_id():
+def _generate_user_profile():
     try:
         os.makedirs(PIPERIDER_USER_HOME, exist_ok=True)
     except Exception:
@@ -39,8 +37,8 @@ def _generate_user_id():
 
     user_id = uuid.uuid4().hex
     with open(PIPERIDER_USER_PROFILE, 'w+') as f:
-        _yml.dump({'user_id': user_id}, f)
-    return user_id
+        _yml.dump({'user_id': user_id, 'anonymous_tracking': True}, f)
+    return dict(user_id=user_id, anonymous_tracking=True)
 
 
 def _obtain_project_info(datasource=None):
@@ -64,6 +62,14 @@ def _obtain_project_info(datasource=None):
         return {}
 
 def log_event(command, params, status):
+    with open(PIPERIDER_USER_PROFILE, 'r') as f:
+        user_profile = _yml.load(f)
+    # TODO: default anonymous_tracking to false if field is not present
+    tracking = user_profile.get('anonymous_tracking', False)
+    tracking = tracking and isinstance(tracking, bool)
+    if not tracking:
+        return
+
     if not _collector.is_ready():
         return
 
