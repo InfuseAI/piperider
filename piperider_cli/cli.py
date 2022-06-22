f843516e2386dc350912ccdc379bf1a02ceac14f
Even Wei <evenwei@infuseai.io>
2022-06-22 14:27:10 +0800

    Revise by Dave
    Signed-off-by: Even Wei <evenwei@infuseai.io>

--- piperider_cli/cli.py
+++ piperider_cli/cli.py
@@ -63,19 +63,19 @@ def cli():
 cli.command_class = TrackCommand
 
 
-@cli.command(short_help='Show the version information.')
+@cli.command(short_help='Show version information.')
 def version():
-    'Show the version information.'
+    'Show version information.'
     click.echo(__version__)
 
 
-@cli.command(short_help='Initialize PipeRider configurations.')
-@click.option('--no-auto-search', type=click.BOOL, default=False, is_flag=True, help="Disable searching for dbt projects.")
+@cli.command(short_help='Initialize a PipeRider project.')
+@click.option('--no-auto-search', type=click.BOOL, default=False, is_flag=True, help="Disable auto detection of dbt projects.")
 @click.option('--dbt-project-dir', type=click.Path(exists=True), default=None, help='Directory to search for dbt_project.yml.')
 @click.option('--dbt-profiles-dir', type=click.Path(exists=True), default=None, help='Directory to search for dbt profiles.yml.')
 @add_options(debug_option)
 def init(**kwargs):
-    'Initialize PipeRider configurations in interactive mode.'
+    'Initialize a PipeRider project in interactive mode.'
 
     console = Console()
     piperider_config_dir = os.path.join(os.getcwd(), '.piperider')
@@ -110,10 +110,10 @@ def init(**kwargs):
         console.print(config)
 
 
-@cli.command(short_help='Check configurations.')
+@cli.command(short_help='Check project configuration.')
 @add_options(debug_option)
 def diagnose(**kwargs):
-    'Check config, datasource, connections, assertion config and dbt manifest.'
+    'Check project configuration, datasource, connections, assertion config and dbt manifest.'
 
     console = Console()
     console.print('Diagnosing...')
@@ -124,9 +124,9 @@ def diagnose(**kwargs):
         sys.exit(1)
 
 
-@cli.command(short_help='Profile, assert, and generate reports.')
-@click.option('--datasource', default=None, type=click.STRING, help='Datasource to run.', metavar='DATASOURCE_NAME')
-@click.option('--table', default=None, type=click.STRING, help='Table to run.', metavar='TABLE_NAME')
+@cli.command(short_help='Profile data source, run assertions, and generate report(s).')
+@click.option('--datasource', default=None, type=click.STRING, help='Datasource to use.', metavar='DATASOURCE_NAME')
+@click.option('--table', default=None, type=click.STRING, help='Table to use.', metavar='TABLE_NAME')
 @click.option('--output', default=None, type=click.Path(), help='Directory to save the results.')
 @click.option('--no-interaction', is_flag=True, help='Disable interactive mode.')
 @click.option('--skip-report', is_flag=True, help='Skip generating report.')
@@ -134,7 +134,7 @@ def diagnose(**kwargs):
 @click.option('--skip-dbt', is_flag=True, help='Skip running dbt.')
 @add_options(debug_option)
 def run(**kwargs):
-    'Profile, assert, and generate reports.'
+    'Profile data source, run assertions, and generate report(s).'
 
     datasource = kwargs.get('datasource')
     table = kwargs.get('table')
@@ -168,7 +168,7 @@ def generate_report(**kwargs):
 @click.option('--input', default=None, type=click.Path(exists=True), help='Specift the report file to be compared.')
 @add_options(debug_option)
 def compare_report(**kwargs):
-    'Compare two existing reports in interactive mode or specified reports.'
+    'Compare two existing reports selected in interactive mode or by option.'
 
     a = kwargs.get('base')
     b = kwargs.get('input')
