aaf4419a1aee8205f41c8f49a03cf3d4a7a4a085
Kent Huang <kent@infuseai.io>
2022-06-23 17:33:53 +0800

    [Feature] sc-26477 update recommended assertion rules
     - Modify the logic of generate min test functions
     - Add 2 build-in test functions. `assert_row_count` and
       `assert_column_schema_type`
     - Add 2 recommended assertion rules to create test case for
       `assert_row_count` and `assert_column_schema_type`
    
    Signed-off-by: Kent Huang <kent@infuseai.io>
    Co-authored-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- piperider_cli/assertion_engine/__init__.py
+++ piperider_cli/assertion_engine/__init__.py
@@ -24,9 +24,59 @@ def assert_row_count_in_range(context: AssertionContext, table: str, column: str
         return context.result.fail_with_assertion_error('The range should be integers.')
 
     if between_criteria[0] > between_criteria[1]:
-        return context.result.fail_with_assertion_error('The minimun value of the range should be the first number.')
+        return context.result.fail_with_assertion_error('The minimum value of the range should be the first number.')
 
     if between_criteria[0] <= row_count <= between_criteria[1]:
+        return context.result.success()
+
+    return context.result.fail()
+
+
+def assert_row_count(context: AssertionContext, table: str, column: str, metrics: dict) -> AssertionResult:
+    table_metrics = metrics.get('tables', {}).get(table)
+    if not table_metrics:
+        return context.result.fail_with_metric_not_found_error(context.table, context.column)
+
+    # Get the metric for the current table
+    row_count = table_metrics.get('row_count')
+    context.result.actual = row_count
+
+    min = context.asserts.get('min', 0)
+    max = context.asserts.get('max')
+
+    if context.asserts.get('min') is None and context.asserts.get('max') is None:
+        return context.result.fail_with_assertion_error('Expect a min or max value.')
+    if not isinstance(min, int):
+        return context.result.fail_with_assertion_error('The min value should be an integer.')
+    if min < 0:
+        return context.result.fail_with_assertion_error('The min value should be greater than or equal to 0.')
+
+    # Only provide min
+    if max is None:
+        if min <= row_count:
+            return context.result.success()
+    else:
+        if not isinstance(max, int):
+            return context.result.fail_with_assertion_error('The max value should be an integer.')
+        if min <= row_count <= max:
+            return context.result.success()
+    return context.result.fail()
+
+
+def assert_column_schema_type(context: AssertionContext, table: str, column: str, metrics: dict) -> AssertionResult:
+    column_metrics = metrics.get('tables', {}).get(table, {}).get('columns', {}).get(column)
+    if not column_metrics:
+        return context.result.fail_with_metric_not_found_error(context.table, context.column)
+
+    # Check assertion input
+    assert_schema_type = context.asserts.get('schema_type').upper()
+    if not assert_schema_type:
+        return context.result.fail_with_assertion_error(f'Expect a SQL schema type')
+
+    schema_type = column_metrics.get('schema_type')
+    context.result.actual = schema_type
+
+    if schema_type == assert_schema_type:
         return context.result.success()
 
     return context.result.fail()
