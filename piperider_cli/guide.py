28d9301f386e2f4249764412f077fea89d0dc10d
Ching Yi, Chan <qrtt1@infuseai.io>
2022-06-28 15:20:39 +0800

    Update the next-step wording
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- piperider_cli/guide.py
+++ piperider_cli/guide.py
@@ -1,17 +1,21 @@
 import os
 
 from click import Context
+from rich.console import Console
 
 from piperider_cli.configuration import PIPERIDER_WORKSPACE_NAME
 
 piperider_config = os.path.join(os.getcwd(), PIPERIDER_WORKSPACE_NAME, 'config.yml')
 report_directory = os.path.join(os.getcwd(), PIPERIDER_WORKSPACE_NAME, 'outputs')
 
+console = Console()
 
-def show(text):
-    print()
-    print("Next Steps:")
-    print(text)
+
+def show(description):
+    console.line()
+    console.print("Next step:")
+    console.print(f'  {description}')
+    console.line()
 
 
 def piperider_initialized() -> bool:
@@ -38,22 +42,25 @@ class Guide(object):
     def show_tips(self, ctx: Context):
         command_name = ctx.command.name
 
+        if command_name == 'version':
+            return
+
         if command_name != 'init' and not piperider_initialized():
-            show('* init')
+            show("Piperider is not initialized. Please execute command 'piperider init' to move forward.")
             return
 
         if command_name == 'init':
-            show('* diagnose')
+            show("Please execute command 'piperider diagnose' to verify configuration")
             return
 
         if command_name == 'diagnose' and number_of_reports() < 2:
-            show('* run')
+            show("Please execute command 'piperider run' to generate your first report")
             return
 
         if command_name == 'run' and number_of_reports() == 1:
-            show('* run')
+            show("Please execute command 'piperider run' to generate your second report")
             return
 
         if command_name == 'run' and number_of_reports() == 2:
-            show('* compare-reports')
+            show("Please execute command 'piperider compare-reports' to get the comparison report")
             return
