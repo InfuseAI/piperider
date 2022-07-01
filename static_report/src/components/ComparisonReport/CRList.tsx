dd5c251bb8a67fe5d287e1dc8b6c13bf3a7bcba1
Jie Peng <im@jiepeng.me>
2022-07-01 11:00:44 +0800

    feat(dbt): added dbt assertions into list pagee
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- static_report/src/components/ComparisonReport/CRList.tsx
+++ static_report/src/components/ComparisonReport/CRList.tsx
@@ -4,6 +4,7 @@ import {
   Table,
   TableContainer,
   Tbody,
+  Text,
   Td,
   Th,
   Thead,
@@ -15,7 +16,7 @@ import { Link } from 'wouter';
 import { nanoid } from 'nanoid';
 
 import {
-  getReportAsserationStatusCounts,
+  getComparisonAssertionTests,
   formatReportTime,
   formatNumber,
   nestComparisonValueByKey,
@@ -23,8 +24,13 @@ import {
 
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
 import type { AssertionResult } from '../../types';
+import type { ComparisonReportSchema } from '../../sdlc/comparison-report-schema';
 
-export function ComparisonReportList({ data }) {
+export function ComparisonReportList({
+  data,
+}: {
+  data: ComparisonReportSchema;
+}) {
   const { base, input } = data;
 
   const tables = nestComparisonValueByKey(base.tables, input.tables);
@@ -96,27 +102,63 @@ export function ComparisonReportList({ data }) {
               <Thead>
                 <Tr>
                   <Th>Name</Th>
-                  <Th>Passed</Th>
-                  <Th>Failed</Th>
+                  <Th>PipeRider</Th>
+                  <Th>dbt</Th>
                   <Th>Rows</Th>
                   <Th>Columns</Th>
+                </Tr>
+                <Tr>
+                  <Th />
+                  <Th>
+                    <Text as="span" mr={8}>
+                      Passed
+                    </Text>
+                    <Text as="span">Failed</Text>
+                  </Th>
+                  <Th>
+                    <Text as="span" mr={8}>
+                      Passed
+                    </Text>
+                    <Text as="span">Failed</Text>
+                  </Th>
+                  <Th />
+                  <Th />
                 </Tr>
               </Thead>
               <Tbody>
                 {Object.keys(tables).map((key) => {
                   const table = tables[key];
-                  const baseOverview = getReportAsserationStatusCounts(
-                    // TODO: upstream types can improve
-                    table.base?.assertion_results
+
+                  const baseOverview = getComparisonAssertionTests({
+                    // TODO: improve upstream type
+                    assertion: table.base.assertion_results
                       ? (table.base.assertion_results as AssertionResult)
                       : undefined,
-                  );
-                  const inputOverview = getReportAsserationStatusCounts(
-                    // TODO: upstream types can improve
-                    table.input?.assertion_results
+                    from: 'base',
+                  });
+                  const inputOverview = getComparisonAssertionTests({
+                    // TODO: improve upstream type
+                    assertion: table.input.assertion_results
                       ? (table.input.assertion_results as AssertionResult)
                       : undefined,
-                  );
+                    from: 'input',
+                  });
+
+                  // If running by `piperider run --dbt-test`, it will have this field, vice versa.
+                  const dbtBaseOverview = getComparisonAssertionTests({
+                    // TODO: improve upstream type
+                    assertion: table.base.dbt_test_results
+                      ? (table.base.dbt_test_results as AssertionResult)
+                      : undefined,
+                    from: 'base',
+                  });
+                  const dbtInputOverview = getComparisonAssertionTests({
+                    // TODO: improve upstream type
+                    assertion: table.input.dbt_test_results
+                      ? (table.input.dbt_test_results as AssertionResult)
+                      : undefined,
+                    from: 'input',
+                  });
 
                   return (
                     <Link key={nanoid()} href={`/tables/${key}`}>
@@ -126,20 +168,34 @@ export function ComparisonReportList({ data }) {
                       >
                         <Td>{key}</Td>
                         <Td>
-                          {baseOverview.passed}
-                          {' | '}
-                          {inputOverview.passed}
+                          <Text as="span">{baseOverview.passed}</Text>
+                          {' / '}
+                          <Text as="span" mr={10}>
+                            {inputOverview.passed}
+                          </Text>
+
+                          <Text as="span">{baseOverview.failed}</Text>
+                          {' / '}
+                          <Text as="span">{inputOverview.failed}</Text>
                         </Td>
+
                         <Td>
-                          {baseOverview.failed}
-                          {' | '}
-                          {inputOverview.failed}
+                          <Text as="span">{dbtBaseOverview.passed}</Text>
+                          {' / '}
+                          <Text as="span" mr={10}>
+                            {dbtInputOverview.passed}
+                          </Text>
+
+                          <Text as="span">{dbtBaseOverview.failed}</Text>
+                          {' / '}
+                          <Text as="span">{dbtInputOverview.failed}</Text>
                         </Td>
+
                         <Td>
                           {table.base?.row_count
                             ? formatNumber(table.base.row_count)
                             : '-'}
-                          {' | '}
+                          {' / '}
                           {table.input?.row_count
                             ? formatNumber(table.input.row_count)
                             : '-'}
@@ -148,7 +204,7 @@ export function ComparisonReportList({ data }) {
                           {table.base?.col_count
                             ? formatNumber(table.base.col_count)
                             : '-'}
-                          {' | '}
+                          {' / '}
                           {table.input?.col_count
                             ? formatNumber(table.input?.col_count)
                             : '-'}
