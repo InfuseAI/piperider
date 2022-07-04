9ca3788727f30cf65b03eaf306f42787f482c9e1
Ching Yi, Chan <qrtt1@infuseai.io>
2022-07-04 15:03:51 +0800

    Show most-common value at string column
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>
    Co-authored-by: Timothy Lee <ctiml@infuseai.io>

--- static_report/src/components/SingleReport/SRTableColumnDetails.tsx
+++ static_report/src/components/SingleReport/SRTableColumnDetails.tsx
@@ -2,6 +2,7 @@ import { Code, Flex, Text } from '@chakra-ui/react';
 import { MetricsInfo } from '../shared/MetrisInfo';
 import { SingleReportSchema } from '../../sdlc/single-report-schema';
 import { formatNumber, getColumnDetails, getMissingValue } from '../../utils';
+import { zip } from 'lodash';
 
 // FIXME: Temp Typing
 type SRTableColumnDetailsProps = {
@@ -11,6 +12,39 @@ type SRTableColumnDetailsProps = {
 export const SRTableColumnDetails = ({ column }: SRTableColumnDetailsProps) => {
   const { mismatch, mismatchOfTotal, missing, valid, validOfTotal } =
     getColumnDetails(column);
+
+  const toMostCommon = (
+    column: SingleReportSchema['tables']['ACTION']['columns']['symbol'],
+  ) => {
+    // show the most common values
+    // * give null if type mismatch
+    // * skip null value
+    // * show top 2 if the values share the same counting, examples:
+    //    (more than 2) a:100, b:100, c:100 => a, b, ...
+    //    (2) a:100, b:100 => a
+    //    (2) null:100, a:100, b:100 => a, b
+    //    (2) null:101, a:100, b:100 => a, b
+    //    (2) a:100, b:100 => a, b
+    //    (1) a:100 => a
+    //    (1) a:100, b:99, c:99 => a
+
+    if (column.type !== 'string') {
+      return null;
+    }
+
+    const data = zip(column.distribution.labels, column.distribution.counts)
+      .filter((x) => x[0] !== null)
+      .slice(0, 3);
+    const topCount = data[0][1];
+    const tops = data.filter((x) => x[1] == topCount).map((x) => x[0]);
+
+    if (tops.length > 2) {
+      return tops.slice(0, 2).join(', ') + ', ...';
+    }
+    return tops.join(', ');
+  };
+
+  const mostCommon = toMostCommon(column);
 
   return (
     <Flex direction="column" gap={3}>
@@ -54,6 +88,12 @@ export const SRTableColumnDetails = ({ column }: SRTableColumnDetailsProps) => {
       <Flex direction="column" mt={3}>
         <MetricsInfo name="Distinct" base={formatNumber(column.distinct)} />
       </Flex>
+
+      {column.type === 'string' && mostCommon && (
+        <Flex direction="column">
+          <MetricsInfo name="Most common" base={mostCommon} />
+        </Flex>
+      )}
 
       {column.type === 'numeric' && (
         <Flex direction="column">
