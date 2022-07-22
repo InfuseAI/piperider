83dfd5e46873ec68f8b2c73737b2a9d2154bcde9
Wei-Chun, Chang <wcchang@infuseai.io>
2022-07-22 17:12:39 +0800

    fix: adjust equal comparing of case when statement
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- piperider_cli/profiler/profiler.py
+++ piperider_cli/profiler/profiler.py
@@ -502,7 +502,9 @@ class NumericColumnProfiler(BaseColumnProfiler):
             if i != num_buckets - 1:
                 cases += [(column < bound, i)]
             else:
-                cases += [(column <= bound, i)]
+                cases += [(column < bound, i)]
+                # equal comparing
+                cases += [(func.abs(column - bound) < interval / 100, i)]
 
         cte_with_bucket = select([
             column.label("c"),
