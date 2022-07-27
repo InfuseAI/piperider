985014957932978c9edd3d44a4ce5d5a6c69d162
Wei-Chun, Chang <wcchang@infuseai.io>
2022-07-27 17:31:54 +0800

    review change for separating semantic issue
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>
    Co-authored-by: popcorny <celu@infuseai.io>

--- piperider_cli/assertion_engine/recommended_rules/table_assertions.py
+++ piperider_cli/assertion_engine/recommended_rules/table_assertions.py
@@ -34,8 +34,8 @@ def recommended_column_min_assertion(table, column, profiling_result) -> Recomme
     column_metric = profiling_result['tables'][table]['columns'][column]
     column_type = column_metric['type']
     if column_type == 'numeric':
-        valids = column_metric['valid']
-        if not valids:
+        total = column_metric['total']
+        if not total:
             return None
 
         column_min = column_metric['min']
@@ -47,7 +47,7 @@ def recommended_column_min_assertion(table, column, profiling_result) -> Recomme
             if i == len(distribution_counts) // 2:
                 break
 
-        if count / valids > 0.95:
+        if count / total > 0.95:
             test_function_name = 'assert_column_min_in_range'
             assertion_values = {
                 'min': sorted([round(column_min * 0.9, 4), round(column_min * 1.1, 4)])
@@ -65,8 +65,8 @@ def recommended_column_max_assertion(table, column, profiling_result) -> Recomme
     column_metric = profiling_result['tables'][table]['columns'][column]
     column_type = column_metric['type']
     if column_type == 'numeric':
-        valids = column_metric['valid']
-        if not valids:
+        total = column_metric['total']
+        if not total:
             return None
 
         column_max = column_metric['max']
@@ -78,7 +78,7 @@ def recommended_column_max_assertion(table, column, profiling_result) -> Recomme
             if i == len(distribution_counts) // 2:
                 break
 
-        if count / valids > 0.95:
+        if count / total > 0.95:
             test_function_name = 'assert_column_max_in_range'
             assertion_values = {
                 'max': sorted([round(column_max * 0.9, 4), round(column_max * 1.1, 4)])
