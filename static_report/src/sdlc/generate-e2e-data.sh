6a42c166cad5e86a3500de4e71e1040fa3a5fdb4
jonycfu <jonycfu@infuseai.io>
2022-08-04 09:36:09 +0800

    fix: e2e automation (new cases added)

--- static_report/src/sdlc/generate-e2e-data.sh
+++ static_report/src/sdlc/generate-e2e-data.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 # For generating comparison_data.json from `piperider-getting-started`
 # NOTE: This file assumes you are calling from the root of repo
+# NOTE: Pass parameters to shell call e.g. $1
 
-# clone repo
-git clone https://github.com/InfuseAI/piperider-getting-started.git
+rm -rf piperider-getting-started
 
-cd piperider-getting-started
+# First 2 run.jsons
+if [ $1 = "first" ]; then
+	echo "initializing...$1"
 
-# Fetch data sqlite - First run.json #1
-curl -o data/sp500.db https://piperider-data.s3.ap-northeast-1.amazonaws.com/getting-started/sp500_20220401.db
+	git clone https://github.com/InfuseAI/piperider-getting-started.git
+	cd piperider-getting-started
 
-piperider run --no-interaction --debug #first run
+	# Fetch data sqlite - First run.json #1
+	curl -o data/sp500.db https://piperider-data.s3.ap-northeast-1.amazonaws.com/getting-started/sp500_20220401.db
 
-# Fetch data sqlite - Second run.json #2
-curl -o data/sp500.db https://piperider-data.s3.ap-northeast-1.amazonaws.com/getting-started/sp500_20220527.db
+	piperider run --no-interaction --debug #first run
 
-piperider run --no-interaction --debug #second run
+	# Fetch data sqlite - Second run.json #2
+	curl -o data/sp500.db https://piperider-data.s3.ap-northeast-1.amazonaws.com/getting-started/sp500_20220527.db
+
+	piperider run --no-interaction --debug #second run
+fi
+
+# edge-cases (another run.json)
+if ["$1" = "second"]; then
+	echo "initializing...$1"
+	# Fetch data sqlite - Third run.json #3 (diff table)
+	curl -o data/sp500.db https://piperider-data.s3.ap-northeast-1.amazonaws.com/integration-test-sqlite/profiler-e2e.db
+
+	piperider run --no-interaction --debug # third run
+fi
 
 # Gather base/target refs for comparison-report
 E2E_SR_REPORTS="./.piperider/outputs/*"
-ARR_PATHS=();
-for FILE_PATH in $E2E_SR_REPORTS; do 
+ARR_PATHS=()
+for FILE_PATH in $E2E_SR_REPORTS; do
 	ARR_PATHS+=($FILE_PATH)
 done
-
-# generate comparison report
+# generate comparison report (base: latest, target: previous )
 piperider compare-reports --base ${ARR_PATHS[0]}/run.json --target ${ARR_PATHS[1]}/run.json --debug
+
 cd ..
