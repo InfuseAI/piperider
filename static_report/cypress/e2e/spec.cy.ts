10e67775e4c04769834b4a6a9dba96521a0dd3f8
jonycfu <jonycfu@infuseai.io>
2022-08-04 10:52:37 +0800

    fix esmodule warning

--- static_report/cypress/e2e/spec.cy.ts
+++ static_report/cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('Home Page', () => {
-  it('Navigate thru SR Pages', () => {
+  it('Navigate thru Normal SR Pages', () => {
     // ARRANGE
     cy.visit('http://localhost:3000');
 
@@ -26,7 +26,7 @@ describe('Home Page', () => {
     // Return to Index
     cy.get('[data-cy="sr-report-breadcrumb-back"]').click();
   });
-  it('Navigate thru CR Pages', () => {
+  it('Navigate thru Normal CR Pages', () => {
     // ARRANGE
     cy.visit('http://localhost:3001');
 
@@ -54,4 +54,61 @@ describe('Home Page', () => {
     // Return to Index
     cy.get('[data-cy="cr-report-breadcrumb-back"]').click();
   });
+  it('Navigate thru Abnormal Datasets SR Pages', () => {
+    // ARRANGE
+    cy.visit('http://localhost:4000');
+
+    // ACT (None: Loading only)
+
+    // ASSERT
+    // SROverview
+
+    // SRList
+    const list = cy.get('[data-cy="sr-report-list"]');
+    list.should('be.visible');
+    const listItem = cy
+      .get('[data-cy="sr-report-list-item"]')
+      .should('be.visible');
+    // Click to navigate...
+    listItem.first().click();
+    // Check navigation success...
+    cy.url().should('include', '/tables/T_BOOL');
+
+    // SingleReport
+    // Click tabs...
+    cy.get('[data-cy="sr-report-tab-item"]').click();
+
+    // Return to Index
+    cy.get('[data-cy="sr-report-breadcrumb-back"]').click();
+  });
+  it('Navigate thru Asymmetric Column CR Pages', () => {
+    // ARRANGE
+    cy.visit('http://localhost:4001');
+
+    // ACT (None: Loading only)
+
+    // ASSERT
+    // CROverview
+
+    // CRList
+    const list = cy.get('[data-cy="cr-report-list"]');
+    list.should('be.visible');
+    const listItem = cy
+      .get('[data-cy="cr-report-list-item"]')
+      .should('be.visible');
+    // Click to navigate...
+    listItem.first().click();
+    // Check navigation success...
+    cy.url().should('include', '/tables/T_BOOL');
+
+    // ComparisonReport
+    // Click tabs...
+    cy.get('[data-cy="cr-report-tab-item-profiling"]').click();
+    cy.get('[data-cy="cr-report-tab-item-tests"]').click();
+
+    // Return to Index
+    cy.get('[data-cy="cr-report-breadcrumb-back"]').click();
+  });
 });
+
+export {}; // fix esmodule warning
