76f1130a91f94fe2496ed94097833c51475512c2
jonycfu <jonycfu@infuseai.io>
2022-08-15 10:08:54 +0800

    revert to category type (histogram); Impl abstract axis

--- static_report/src/components/shared/Charts/HistogramChart.tsx
+++ static_report/src/components/shared/Charts/HistogramChart.tsx
@@ -1,13 +1,14 @@
 import {
   ChartOptions,
   Chart as ChartJS,
-  LinearScale,
   BarElement,
   Tooltip,
   ChartData,
   ScaleOptionsByType,
   CartesianScaleTypeRegistry,
   TimeSeriesScale,
+  CategoryScale,
+  LinearScale,
 } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
 import { ColumnSchema, Histogram } from '../../../sdlc/single-report-schema';
@@ -24,7 +25,13 @@ import {
 import 'chartjs-adapter-date-fns';
 import { DeepPartial } from 'chart.js/types/utils';
 
-ChartJS.register(TimeSeriesScale, LinearScale, BarElement, Tooltip);
+ChartJS.register(
+  TimeSeriesScale,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  Tooltip,
+);
 /**
  * Histogram Chart that can display generic data types such as Numeric, Datetime, Integer
  * X: The Min/Max of the labels range is the scaled width of charting area
@@ -38,14 +45,15 @@ interface Props {
 }
 export function HistogramChart({ data, type, total }: Props) {
   const { counts, bin_edges } = data;
-
-  const newData = counts.map((v, i) => ({ x: bin_edges[i + 1], y: v }));
   const isDatetime = type === 'datetime';
-  // to make histogram edge display on tick
-  const labelOffset = bin_edges.length < 11 ? 120 / bin_edges.length : 0;
+
+  // Time series (x,y) plot when type is datetime, else category series (bin_edges[i], counts)
+  const newData = isDatetime
+    ? counts.map((v, i) => ({ x: bin_edges[i], y: v }))
+    : counts;
 
   //swap x-scale when histogram is datetime
-  const xScale: DeepPartial<
+  const xScaleBase: DeepPartial<
     ScaleOptionsByType<keyof CartesianScaleTypeRegistry>
   > = isDatetime
     ? {
@@ -57,25 +65,34 @@ export function HistogramChart({ data, type, total }: Props) {
           minUnit: 'day',
         },
         bounds: 'data',
-        grid: { display: true, offset: true },
-        ticks: {
-          maxRotation: 30,
-          autoSkip: true,
-        },
-      }
-    : {
-        type: 'linear',
-        bounds: 'data',
+        min: bin_edges[0],
         grid: { display: false },
         ticks: {
           maxRotation: 30,
-          autoSkip: true,
-          labelOffset,
-          callback(val) {
-            return formatAsAbbreviatedNumber(val);
+        },
+      }
+    : {
+        type: 'category',
+        offset: false, // offsets against `x2` abstract axis (not visible)
+        grid: { display: false },
+        ticks: {
+          maxRotation: 30,
+          callback(val, index) {
+            const interpretedVal = this.getLabelForValue(index);
+            return formatAsAbbreviatedNumber(interpretedVal);
           },
         },
       };
+  // For aligning category type (non-datetime) only -- not visible
+  const xScaleCategoryAxis: DeepPartial<
+    ScaleOptionsByType<keyof CartesianScaleTypeRegistry>
+  > = {
+    display: false,
+    max: bin_edges.length - 1, //based on number of categories, not value!
+    position: 'bottom',
+    grid: { display: true },
+  };
+
   const chartOptions: ChartOptions<'bar'> = {
     responsive: true,
     maintainAspectRatio: false,
@@ -103,7 +120,8 @@ export function HistogramChart({ data, type, total }: Props) {
       },
     },
     scales: {
-      x: xScale,
+      x2: xScaleCategoryAxis,
+      x: xScaleBase,
       y: {
         beginAtZero: true,
         grid: {
@@ -122,6 +140,7 @@ export function HistogramChart({ data, type, total }: Props) {
   // Histograms are a mix between bar and scatter options (union)
   // uses cartesian x,y coordinates to plot and infer labels
   const chartData: ChartData<'bar' | 'scatter'> = {
+    labels: bin_edges,
     datasets: [
       {
         data: newData as any, //infer `any` to allow datestring
@@ -131,6 +150,7 @@ export function HistogramChart({ data, type, total }: Props) {
         borderWidth: 1,
         categoryPercentage: 1, // tells bar to fill "bin area"
         barPercentage: 1, //tells bar to fill "bar area"
+        xAxisID: isDatetime ? 'x' : 'x2', // non-datetime is treated as category type, so requires diff axis alignments
       },
     ],
   };
