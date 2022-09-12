1e81f3206acfd0ce44becc9ec5296cef047a2e63
jonycfu <jonycfu@infuseai.io>
2022-09-12 09:09:22 +0800

    fix: rollup build (chart.js <3.9.0, due to .mts support)

---
+++ static_report/rollup.config.js
@@ -0,0 +1,39 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+const packageJson = require('./package.json');
+
+// rollup.config.js
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const rollupConfig = [
+  {
+    input: 'src/lib.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
+export default rollupConfig;
