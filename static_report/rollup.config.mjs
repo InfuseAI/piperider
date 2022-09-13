7cfc1945be4e48f456306830ec28428a37f4abb7
jonycfu <jonycfu@infuseai.io>
2022-09-13 15:22:03 +0800

    fix(wip): lib compiles, but breaks on element-emotions

--- static_report/rollup.config.mjs
+++ static_report/rollup.config.mjs
@@ -3,9 +3,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 
 // rollup.config.js
 /**
@@ -27,6 +28,7 @@ const rollupConfig = [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({
@@ -36,6 +38,23 @@ const rollupConfig = [
       terser(),
       nodePolyfills(),
     ],
+    // external: [
+    //   'react',
+    //   'react-chartjs-2',
+    //   'react-dom',
+    //   'react-icons',
+    //   '@chakra-ui/icons',
+    //   '@chakra-ui/react',
+    //   '@chakra-ui/styled-system',
+    //   '@chakra-ui/system',
+    //   '@chakra-ui/theme-tools',
+    //   '@emotion/react',
+    //   '@emotion/styled',
+    //   '@sgratzl/chartjs-chart-boxplot',
+    //   'chart.js',
+    //   'chartjs-adapter-date-fns',
+    //   'framer-motion',
+    // ],
   },
   {
     input: 'dist/esm/index.d.ts',
