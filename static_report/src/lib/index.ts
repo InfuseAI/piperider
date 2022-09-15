300a3ce5ad9f6cc8970c25d35cac7328a7a10179
jonycfu <jonycfu@infuseai.io>
2022-09-15 11:58:40 +0800

    add chakra-ui deps into peer-deps + dev-deps

--- static_report/src/lib/index.ts
+++ static_report/src/lib/index.ts
@@ -2,7 +2,7 @@
  * Main Entry File for UI Library
  */
 export * from '../components/shared';
-export * from '../utils';
-export * from '../hooks';
-export * from '../types';
-export * from '../sdlc';
+// export * from '../utils';
+// export * from '../hooks';
+// export * from '../types';
+// export * from '../sdlc';
