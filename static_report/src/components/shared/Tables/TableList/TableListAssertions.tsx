40ecc8f363eb6a09a7900aeeb70e6279a292f255
jonycfu <jonycfu@infuseai.io>
2022-09-29 09:07:46 +0800

    fix: assertion-label reusage (cleanup variations of table-list-assertion elements)

--- static_report/src/components/shared/Tables/TableList/TableListAssertions.tsx
+++ static_report/src/components/shared/Tables/TableList/TableListAssertions.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Center, Icon } from '@chakra-ui/react';
+import { Text, Center, Icon, Grid } from '@chakra-ui/react';
 import {
   FiCheck,
   FiX,
@@ -11,7 +11,6 @@ import {
   formatColumnValueWith,
   formatNumber,
 } from '../../../../utils/formatters';
-import { NO_VALUE } from '../../Columns';
 
 interface Props {
   baseAssertionTotal: number | string;
@@ -19,150 +18,29 @@ interface Props {
   targetAssertionTotal: number | string;
   targetAssertionFailed: number | string;
 }
-/**
- * SR = e.g. <checkmark> All of 10
- * CR = e.g. All Passed|Failed / 10 total -> 10/10
- */
+
 export function TableListAssertionSummary({
   baseAssertionFailed,
   baseAssertionTotal,
   targetAssertionFailed,
   targetAssertionTotal,
 }: Props) {
-  const failedDelta =
-    typeof baseAssertionFailed === 'number' &&
-    typeof targetAssertionFailed === 'number'
-      ? targetAssertionFailed - baseAssertionFailed
-      : NO_VALUE;
   return (
-    <Flex gap={2} color="gray.500" alignItems="center">
-      {/* base assertions */}
-      <Flex gap={1} alignItems="center">
-        <BaseTableAssertionSummary
-          total={baseAssertionTotal}
-          failed={baseAssertionFailed}
-        />
-        <Text as="span">/</Text>
-        <Text as="span">
-          {formatColumnValueWith(baseAssertionTotal, formatNumber)}
-        </Text>
-      </Flex>
+    <Grid
+      gap={3}
+      templateColumns={'1fr 1em 1fr'}
+      alignItems={'center'}
+      color="gray.500"
+    >
+      <AssertionLabel total={baseAssertionTotal} failed={baseAssertionFailed} />
 
       <Icon as={FiArrowRight} />
 
-      {/* target assertions */}
-      <Flex gap={1} alignItems="center">
-        <TargetTableAssertion
-          total={targetAssertionTotal}
-          failed={targetAssertionFailed}
-          failedDelta={failedDelta}
-        />
-        <Text as="span">/</Text>
-        <TargetTableAssertionsDelta
-          baseAssertions={baseAssertionTotal}
-          targetAssertions={targetAssertionTotal}
-        />
-      </Flex>
-    </Flex>
-  );
-}
-
-function BaseTableAssertionSummary({
-  total,
-  failed,
-}: {
-  total: number | string;
-  failed: number | string;
-}) {
-  if (total === 0) {
-    return (
-      <Text as="span" color="gray.500">
-        0
-      </Text>
-    );
-  }
-
-  if (total > 0 && failed === 0) {
-    return (
-      <Text as="span" color="#2CAA00">
-        All Passed
-      </Text>
-    );
-  }
-
-  if (failed > 0)
-    return (
-      <Text as="span" color="#F60059">
-        {formatColumnValueWith(failed, formatNumber)}
-      </Text>
-    );
-  return <Text>{NO_VALUE}</Text>;
-}
-
-function TargetTableAssertion({
-  total,
-  failed,
-  failedDelta,
-}: {
-  total: number | string;
-  failed: number | string;
-  failedDelta: number | string;
-}) {
-  if (total === 0) {
-    return (
-      <Text as="span" color="gray.500">
-        none
-      </Text>
-    );
-  }
-
-  if (failedDelta !== 0 && typeof failedDelta === 'number') {
-    return (
-      <Center gap={1} color="#F60059">
-        <AssertionsLabelIcon delta={failedDelta} />
-        <Text as="span">
-          {formatColumnValueWith(Math.abs(failedDelta), formatNumber)}
-        </Text>
-      </Center>
-    );
-  }
-
-  // When `failedDelta = 0`, check `failed` number if is `0` then rendering `total`
-  return (
-    <Text as="span">
-      {failed === 0
-        ? formatColumnValueWith(total, formatNumber)
-        : formatColumnValueWith(failed, formatNumber)}
-    </Text>
-  );
-}
-
-function TargetTableAssertionsDelta({
-  baseAssertions,
-  targetAssertions,
-}: {
-  baseAssertions: number | string;
-  targetAssertions: number | string;
-}) {
-  const delta =
-    typeof baseAssertions === 'number' && typeof targetAssertions === 'number'
-      ? targetAssertions - baseAssertions
-      : NO_VALUE;
-  const isGreaterThanZero = delta > 0;
-
-  if (targetAssertions === 0) {
-    return <Text as="span">none</Text>;
-  }
-
-  return (
-    <Center gap={1}>
-      {typeof delta === 'number' && delta !== 0 ? (
-        <AssertionsLabelIcon delta={delta} />
-      ) : null}
-      <Text as="span" color={isGreaterThanZero ? 'black' : 'inherit'}>
-        {formatColumnValueWith(targetAssertions, formatNumber)}
-      </Text>
-    </Center>
+      <AssertionLabel
+        total={targetAssertionTotal}
+        failed={targetAssertionFailed}
+      />
+    </Grid>
   );
 }
 
