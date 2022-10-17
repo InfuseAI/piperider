a1f70e22768a802a3453d047abff9ad34690a3b4
jonycfu <jonycfu@infuseai.io>
2022-10-17 08:31:04 +0800

    remove: table-col-summ-list (old UI)

--- static_report/src/components/shared/Tables/TableList/index.ts
+++ static_report/src/components/shared/Tables/TableList/index.ts
@@ -1,7 +1,6 @@
 export * from './ColumnBadge';
 export * from './ColumnName';
 export * from './TableColumnSchemaList';
-export * from './TableColumnSummaryList';
 export * from './TableListAssertions';
 export * from './TableListItem';
 export * from './TableRowColDeltaSummary';
