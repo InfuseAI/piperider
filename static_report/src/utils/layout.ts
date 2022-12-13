1e328d742e2027c3fdcfc14c6562664e81e1ddce
Jie Peng <im@jiepeng.me>
2022-12-13 11:19:00 +0800

    revert: layout
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- static_report/src/utils/layout.ts
+++ static_report/src/utils/layout.ts
@@ -3,7 +3,7 @@ export const breadcrumbHeight = 48;
 export const mainContentAreaHeight = `calc(100vh - ${
   topNavAndFooterHeightOffset + breadcrumbHeight
 }px)`;
-export const tableListGridTempCols = '1fr 1fr 1.5fr';
+export const tableListGridTempCols = '2fr 1fr 1.5fr';
 export const tableListWidth = 1150;
 export const tableListMaxWidth = `calc(${tableListWidth}px - 30px)`;
 export const assertionListWidth = 1280;
