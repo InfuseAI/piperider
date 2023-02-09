3a018421fc9584c0129bce9f3256a0ca6e2efd75
jonycfu <jonycfu@infuseai.io>
2023-02-09 09:19:49 +0800

    fix: cr table-list

--- static_report/src/pages/CRTableListPage.tsx
+++ static_report/src/pages/CRTableListPage.tsx
@@ -7,12 +7,17 @@ import { Flex, Text, Grid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useLocation } from 'wouter';
 
-import { tableListGridTempCols, tableListWidth } from '../utils/layout';
+import {
+  tableListGridTempCols,
+  tableListMaxWidth,
+  tableListWidth,
+} from '../utils/layout';
 import { useReportStore } from '../utils/store';
 import { TableColumnSchemaList } from '../components/Tables/TableList/TableColumnSchemaList';
 import { useAmplitudeOnMount } from '../hooks';
 import { AMPLITUDE_EVENTS, CR_TYPE_LABEL } from '../utils/amplitudeEvents';
 import { CommonModal } from '../components/Common/CommonModal';
+import { MasterDetailContainer } from '../components/Common/MasterDetailContainer';
 
 type Props = { data: ComparisonReportSchema };
 
@@ -30,60 +35,77 @@ export function CRTablesListPage({ data }: Props) {
   const [, setLocation] = useLocation();
   const setReportData = useReportStore((s) => s.setReportRawData);
   setReportData({ base: data.base, input: data.input });
-  const { tableColumnsOnly: tableColEntries = [], assertionsOnly } =
-    useReportStore.getState();
+  const {
+    tableColumnsOnly = [],
+    assertionsOnly,
+    rawData,
+  } = useReportStore.getState();
 
   return (
     <Main isSingleReport={false}>
-      <Flex direction="column" width={tableListWidth} minHeight="650px" pt={9}>
-        <Grid templateColumns={tableListGridTempCols} px={4} my={6}>
-          <Text>Name</Text>
-          <Text>Summary</Text>
-          <Text ml={'5em'}>Assertions</Text>
-        </Grid>
-
-        {tableColEntries.map((tableColEntry, i) => {
-          return (
-            <Flex key={i}>
-              <TableListItem
-                combinedAssertions={assertionsOnly}
-                combinedTableEntry={tableColEntry}
-                onSelect={() =>
-                  setLocation(`/tables/${tableColEntry[0]}/columns/`)
-                }
-                onInfoClick={() => {
-                  setTableColsEntryId(i);
-                  modal.onOpen();
-                }}
-              />
-            </Flex>
-          );
-        })}
-      </Flex>
-
-      <CommonModal
-        {...modal}
-        size="2xl"
-        title={tableColsEntryId !== -1 && tableColEntries[tableColsEntryId][0]}
-        onClose={() => {
-          setTableColsEntryId(-1);
-          modal.onClose();
-        }}
+      <MasterDetailContainer
+        rawData={rawData}
+        tableColEntries={tableColumnsOnly}
       >
-        <Text fontSize="lg" mb={4}>
-          Description:{' '}
-          {(tableColsEntryId !== -1 &&
-            tableColEntries[tableColsEntryId][1].target?.description) ?? (
-            <Text as="i">No description provided.</Text>
+        <Flex direction="column" width={tableListWidth} minHeight="650px" p={9}>
+          <Grid
+            templateColumns={tableListGridTempCols}
+            maxW={tableListMaxWidth}
+            px={4}
+            my={6}
+          >
+            <Text>Name</Text>
+            <Text>Summary</Text>
+            <Text>Assertions</Text>
+          </Grid>
+
+          {tableColumnsOnly.map((tableColEntry, i) => {
+            return (
+              <Flex key={i}>
+                <TableListItem
+                  combinedAssertions={assertionsOnly}
+                  combinedTableEntry={tableColEntry}
+                  onSelect={() =>
+                    setLocation(`/tables/${tableColEntry[0]}/columns/`)
+                  }
+                  onInfoClick={() => {
+                    setTableColsEntryId(i);
+                    modal.onOpen();
+                  }}
+                />
+              </Flex>
+            );
+          })}
+        </Flex>
+
+        <CommonModal
+          {...modal}
+          size="2xl"
+          title={
+            tableColsEntryId !== -1 && tableColumnsOnly[tableColsEntryId][0]
+          }
+          onClose={() => {
+            setTableColsEntryId(-1);
+            modal.onClose();
+          }}
+        >
+          <Text fontSize="lg" mb={4}>
+            Description:{' '}
+            {(tableColsEntryId !== -1 &&
+              tableColumnsOnly[tableColsEntryId][1].target?.description) ?? (
+              <Text as="i">No description provided.</Text>
+            )}
+          </Text>
+          {tableColsEntryId !== -1 && (
+            <TableColumnSchemaList
+              baseTableEntryDatum={tableColumnsOnly[tableColsEntryId][1].base}
+              targetTableEntryDatum={
+                tableColumnsOnly[tableColsEntryId][1].target
+              }
+            />
           )}
-        </Text>
-        {tableColsEntryId !== -1 && (
-          <TableColumnSchemaList
-            baseTableEntryDatum={tableColEntries[tableColsEntryId][1].base}
-            targetTableEntryDatum={tableColEntries[tableColsEntryId][1].target}
-          />
-        )}
-      </CommonModal>
+        </CommonModal>
+      </MasterDetailContainer>
     </Main>
   );
 }
