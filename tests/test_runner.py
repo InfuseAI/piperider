7f8be676bcf97aa95c70223149aa5f26a03832dc
Wei-Chun, Chang <wcchang@infuseai.io>
2023-03-31 11:05:08 +0800

    Add ut for filter statistics
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,12 +3,15 @@ from unittest import TestCase
 
 from piperider_cli.runner import _filter_subject, get_dbt_profile_subjects
 from piperider_cli.profiler import ProfileSubject
+from piperider_cli.statistics import Statistics
 
 
 class TestRunner(TestCase):
 
     def setUp(self):
         self.dbt_state_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mock_dbt_data")
+        self.statistics = Statistics()
+        self.statistics.reset()
 
     def test_filter_subject(self):
         self.assertTrue(_filter_subject('a', None, None))
@@ -37,6 +40,9 @@ class TestRunner(TestCase):
         self.assertEqual('stg_event', subjects[0].name)
         self.assertEqual('session', subjects[1].name)
 
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(5, self.statistics.statistic['notag'])
+
     def test_dbt_profile_subjects_dbt_resources_and_config(self):
         options = dict(
             view_profile=None,
@@ -56,6 +62,9 @@ class TestRunner(TestCase):
         self.assertEqual(1, len(subjects))
         self.assertEqual('project', subjects[0].name)
 
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(6, self.statistics.statistic['filter'])
+
     def test_dbt_profile_subjects_config_excludes(self):
         options = dict(
             view_profile=None,
@@ -71,6 +80,10 @@ class TestRunner(TestCase):
 
         self.assertEqual(5, len(subjects))
         self.assertNotIn('session', [s.name for s in subjects])
+
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(1, self.statistics.statistic['filter'])
+        self.assertEqual(1, self.statistics.statistic['view'])
 
     def test_dbt_profile_subjects_config_includes(self):
         options = dict(
@@ -88,6 +101,10 @@ class TestRunner(TestCase):
         self.assertEqual(1, len(subjects))
         self.assertEqual('user', subjects[0].name)
 
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(5, self.statistics.statistic['filter'])
+        self.assertEqual(1, self.statistics.statistic['view'])
+
     def test_dbt_profile_subjects_config_view_includes(self):
         options = dict(
             view_profile=True,
@@ -104,6 +121,9 @@ class TestRunner(TestCase):
         self.assertEqual(1, len(subjects))
         self.assertEqual('stg_event', subjects[0].name)
 
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(6, self.statistics.statistic['filter'])
+
     def test_dbt_profile_subjects_config_no_view_includes(self):
         options = dict(
             view_profile=False,
@@ -119,3 +139,7 @@ class TestRunner(TestCase):
 
         self.assertEqual(1, len(subjects))
         self.assertEqual('user', subjects[0].name)
+
+        self.assertEqual(7, self.statistics.statistic['total'])
+        self.assertEqual(5, self.statistics.statistic['filter'])
+        self.assertEqual(1, self.statistics.statistic['view'])
