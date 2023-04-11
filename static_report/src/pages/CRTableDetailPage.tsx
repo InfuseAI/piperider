ed5b49d9d4976d2d3437adff294dff7db8293e5c
popcorny <celu@infuseai.io>
2023-04-11 17:47:22 +0800

    CR for table detail: show full blank block for missing side
    Signed-off-by: popcorny <celu@infuseai.io>

--- static_report/src/pages/CRTableDetailPage.tsx
+++ static_report/src/pages/CRTableDetailPage.tsx
@@ -54,6 +54,111 @@ export default function CRTableDetailPage() {
   const baseDataTable = baseTables[tableName];
   const targetDataTable = targetTables[tableName];
 
+  function SeparateView({
+    title,
+    Comp,
+  }: {
+    title: string;
+    Comp: React.FC<{ tableDatum: any }>;
+  }) {
+    if (baseDataTable === undefined) {
+      return (
+        <Box width="100%">
+          <Text fontSize={'xl'}>{title}</Text>
+          <Divider my={1} />
+          <Comp tableDatum={targetDataTable} />
+        </Box>
+      );
+    } else if (targetDataTable === undefined) {
+      return (
+        <Box width="100%">
+          <Text fontSize={'xl'}>{title}</Text>
+          <Divider my={1} />
+          <Comp tableDatum={baseDataTable} />
+        </Box>
+      );
+    } else {
+      return (
+        <Box width="100%">
+          <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
+            <Text fontSize={'xl'}>{title}</Text>
+            <Divider orientation="vertical" />
+            <Text fontSize={'xl'}>{title}</Text>
+          </Grid>
+          <Divider my={1} />
+          <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
+            <Comp tableDatum={baseDataTable} />
+            <Divider orientation="vertical" />
+            <Comp tableDatum={targetDataTable} />
+          </Grid>
+        </Box>
+      );
+    }
+  }
+
+  function MergedView({
+    title,
+    children,
+  }: {
+    title: string;
+    children: React.ReactNode;
+  }) {
+    function Title() {
+      if (baseDataTable === undefined || targetDataTable === undefined) {
+        return <Text fontSize={'xl'}>{title}</Text>;
+      } else {
+        return (
+          <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
+            <Text fontSize={'xl'}>{title}</Text>
+            <Divider orientation="vertical" />
+            <Text fontSize={'xl'}>{title}</Text>
+          </Grid>
+        );
+      }
+    }
+
+    return (
+      <Box width="100%">
+        <Title />
+        <Divider my={1} />
+        {children}
+      </Box>
+    );
+  }
+
+  function ComparisonContent({ children }: { children: React.ReactNode }) {
+    function EmptyBox() {
+      return (
+        <Box
+          bg="gray.100"
+          width="100%"
+          height="100%"
+          borderWidth="2px"
+          borderColor="lightgray"
+        />
+      );
+    }
+    if (baseDataTable == undefined) {
+      return (
+        <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
+          <EmptyBox />
+          <Divider orientation="vertical" />
+          {children}
+        </Grid>
+      );
+    } else if (targetDataTable == undefined) {
+      return (
+        <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
+          {children}
+          <Divider orientation="vertical" />
+          <EmptyBox />
+        </Grid>
+      );
+    } else {
+      return <>{children}</>;
+    }
+  }
+
   return (
     <Box>
       <TableColumnHeader
@@ -64,30 +169,15 @@ export default function CRTableDetailPage() {
       />
 
       <ComparableGridHeader />
-
-      <VStack spacing={10}>
-        <Box width="100%">
-          <Text fontSize={'xl'}>Table Statistics</Text>
-          <Divider my={1} />
-          <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
-            <TableGeneralStats tableDatum={baseDataTable} />
-            <Divider orientation="vertical" />
-            <TableGeneralStats tableDatum={targetDataTable} />
-          </Grid>
-        </Box>
-        <Box width="100%">
-          <TableColumnSchemaCompList tableEntry={currentTableEntry} />
-        </Box>
-        <Box width="100%">
-          <Text fontSize={'xl'}>Duplicate Rows</Text>
-          <Divider my={1} />
-          <Grid templateColumns={'1fr 1px 1fr'} gap={3}>
-            <DupedTableRowsWidget tableDatum={baseDataTable} />
-            <Divider orientation="vertical" />
-            <DupedTableRowsWidget tableDatum={targetDataTable} />
-          </Grid>
-        </Box>
-      </VStack>
+      <ComparisonContent>
+        <VStack spacing={10}>
+          <SeparateView title="Table Statistics" Comp={TableGeneralStats} />
+          <MergedView title="Schema">
+            <TableColumnSchemaCompList tableEntry={currentTableEntry} />
+          </MergedView>
+          <SeparateView title="Duplicate Rows" Comp={DupedTableRowsWidget} />
+        </VStack>
+      </ComparisonContent>
     </Box>
   );
 }
@@ -125,91 +215,140 @@ function TableColumnSchemaCompList({ tableEntry }) {
     tableEntry;
   const fallbackTable = targetTableColEntry || baseTableColEntry;
 
-  return (
-    <Box>
-      <Text fontSize={'xl'}>Schema</Text>
-      <Divider my={1} />
-
-      <Flex direction="column" width="100%">
-        <TableContainer width="100%">
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th width="25%">Base Column</Th>
-                <Th width="25%" borderRight="1px" borderRightColor="gray.200">
-                  Base Type
-                </Th>
-                <Th width="25%">Traget Column</Th>
-                <Th width="25%">Target Type</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {fallbackTable?.columns.map(
-                ([
-                  key,
-                  { base: baseColumn, target: targetColumn },
-                  metadata,
-                ]) => {
-                  return (
-                    <Tr
-                      key={key}
-                      _hover={{
-                        bg: 'blackAlpha.50',
-                        cursor: 'inherit',
-                      }}
-                      data-cy="table-list-schema-item"
+  const MergedSchema = () => (
+    <Flex direction="column" width="100%">
+      <TableContainer width="100%">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th width="25%">Base Column</Th>
+              <Th width="25%" borderRight="1px" borderRightColor="gray.200">
+                Base Type
+              </Th>
+              <Th width="25%">Traget Column</Th>
+              <Th width="25%">Target Type</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {fallbackTable?.columns.map(
+              ([key, { base: baseColumn, target: targetColumn }, metadata]) => {
+                return (
+                  <Tr
+                    key={key}
+                    _hover={{
+                      bg: 'blackAlpha.50',
+                      cursor: 'inherit',
+                    }}
+                    data-cy="table-list-schema-item"
+                  >
+                    <Td
+                      color={metadata?.mismatched ? 'red.500' : 'inherit'}
+                      maxW={'350px'}
                     >
-                      <Td
-                        color={metadata?.mismatched ? 'red.500' : 'inherit'}
-                        maxW={'350px'}
-                      >
-                        <Text
-                          as="span"
-                          fontSize={'xs'}
-                          noOfLines={1}
-                          whiteSpace="normal"
-                          title={baseColumn?.name ?? NO_VALUE}
-                        >
-                          {baseColumn?.name ?? NO_VALUE}
-                        </Text>
-                      </Td>
-                      <Td
-                        color={metadata?.mismatched ? 'red.500' : 'inherit'}
-                        borderRight="1px"
-                        borderRightColor="gray.200"
-                      >
-                        <Text as={'span'} fontSize={'xs'}>
-                          {baseColumn?.schema_type ?? NO_VALUE}
-                        </Text>
-                      </Td>
-
-                      <Td
-                        color={metadata?.mismatched ? 'red.500' : 'inherit'}
+                      <Text
+                        as="span"
+                        fontSize={'xs'}
+                        noOfLines={1}
                         whiteSpace="normal"
+                        title={baseColumn?.name ?? NO_VALUE}
                       >
-                        <Text
-                          fontSize={'xs'}
-                          as="span"
-                          noOfLines={1}
-                          whiteSpace={'normal'}
-                          title={targetColumn?.name ?? NO_VALUE}
-                        >
-                          {targetColumn?.name ?? NO_VALUE}
-                        </Text>
-                      </Td>
-                      <Td color={metadata?.mismatched ? 'red.500' : 'inherit'}>
-                        <Text as={'span'} fontSize={'xs'}>
-                          {targetColumn?.schema_type ?? NO_VALUE}
-                        </Text>
-                      </Td>
-                    </Tr>
-                  );
-                },
-              )}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Flex>
-    </Box>
+                        {baseColumn?.name ?? NO_VALUE}
+                      </Text>
+                    </Td>
+                    <Td
+                      color={metadata?.mismatched ? 'red.500' : 'inherit'}
+                      borderRight="1px"
+                      borderRightColor="gray.200"
+                    >
+                      <Text as={'span'} fontSize={'xs'}>
+                        {baseColumn?.schema_type ?? NO_VALUE}
+                      </Text>
+                    </Td>
+
+                    <Td
+                      color={metadata?.mismatched ? 'red.500' : 'inherit'}
+                      whiteSpace="normal"
+                    >
+                      <Text
+                        fontSize={'xs'}
+                        as="span"
+                        noOfLines={1}
+                        whiteSpace={'normal'}
+                        title={targetColumn?.name ?? NO_VALUE}
+                      >
+                        {targetColumn?.name ?? NO_VALUE}
+                      </Text>
+                    </Td>
+                    <Td color={metadata?.mismatched ? 'red.500' : 'inherit'}>
+                      <Text as={'span'} fontSize={'xs'}>
+                        {targetColumn?.schema_type ?? NO_VALUE}
+                      </Text>
+                    </Td>
+                  </Tr>
+                );
+              },
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Flex>
   );
+
+  const OneSideSchema = () => (
+    <Flex direction="column" width="100%">
+      <TableContainer width="100%">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th width="50%">Column</Th>
+              <Th width="50%">Type</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {fallbackTable?.columns.map(
+              ([key, { base: baseColumn, target: targetColumn }, metadata]) => {
+                const column = baseColumn || targetColumn;
+                return (
+                  <Tr
+                    key={key}
+                    _hover={{
+                      bg: 'blackAlpha.50',
+                      cursor: 'inherit',
+                    }}
+                    data-cy="table-list-schema-item"
+                  >
+                    <Td maxW={'350px'}>
+                      <Text
+                        as="span"
+                        fontSize={'xs'}
+                        noOfLines={1}
+                        whiteSpace="normal"
+                        title={column?.name ?? NO_VALUE}
+                      >
+                        {column?.name ?? NO_VALUE}
+                      </Text>
+                    </Td>
+                    <Td>
+                      <Text as={'span'} fontSize={'xs'}>
+                        {column?.schema_type ?? NO_VALUE}
+                      </Text>
+                    </Td>
+                  </Tr>
+                );
+              },
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Flex>
+  );
+
+  if (
+    baseTableColEntry?.name === undefined ||
+    targetTableColEntry?.name === undefined
+  ) {
+    return <OneSideSchema />;
+  } else {
+    return <MergedSchema />;
+  }
 }
