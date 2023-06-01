85ba9ff313ce19635b6a06f7fa5fff8008ed5fab
Kent Huang <kent@infuseai.io>
2023-06-01 16:42:57 +0800

    [Fix] change way to pass report in dev env
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- static_report/src/App.tsx
+++ static_report/src/App.tsx
@@ -36,9 +36,7 @@ const SRPage = lazy(() => import('./pages/SRPage'));
 const CRPage = lazy(() => import('./pages/CRPage'));
 
 function AppSingle() {
-  const data = process.env.REACT_APP_SINGLE_REPORT_DATA_JSON
-    ? JSON.parse(process.env.REACT_APP_SINGLE_REPORT_DATA_JSON)
-    : window.PIPERIDER_SINGLE_REPORT_DATA || {};
+  const data = window.PIPERIDER_SINGLE_REPORT_DATA || {};
   const setReportData = useReportStore((s) => s.setReportRawData);
   setReportData({ base: data });
   return (
@@ -51,9 +49,7 @@ function AppSingle() {
 }
 
 function AppComparison() {
-  const data = process.env.REACT_APP_COMPARISON_REPORT_DATA_JSON
-    ? JSON.parse(process.env.REACT_APP_COMPARISON_REPORT_DATA_JSON)
-    : window.PIPERIDER_COMPARISON_REPORT_DATA || {};
+  const data = window.PIPERIDER_COMPARISON_REPORT_DATA || {};
   const setReportData = useReportStore((s) => s.setReportRawData);
   setReportData(data);
   return (
