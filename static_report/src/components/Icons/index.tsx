e0feddf6d32d6e12bab4691f13ae819b00133118
popcorny <celu@infuseai.io>
2023-06-26 12:02:23 +0800

    Add implicit icon
    Signed-off-by: popcorny <celu@infuseai.io>
    Co-authored-by: wcchang <wcchang@infuseai.io>

---
+++ static_report/src/components/Icons/index.tsx
@@ -0,0 +1,26 @@
+export function IconImplicit() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M1.5 1 h13 l.5.5 v13 l-.5.5 h-13 l-.5-.5 v-13l.5-.5zM2 2v4h-1v4h1v4h4v1h4v-1h4v-4h1v-4h-1v-4h-4v-1h-4v1h-4z"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8 11 a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+      />
+
+      <path fill-rule="evenodd" clip-rule="evenodd" d="" />
+    </svg>
+  );
+}
