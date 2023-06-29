4a71beae5837cebecc3c7e97a8e2e1ad0693784e
Ching Yi, Chan <qrtt1@infuseai.io>
2023-06-29 16:50:13 +0800

    Update unique-id resolving strategy (#763)
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- piperider_cli/dbt/list_task.py
+++ piperider_cli/dbt/list_task.py
@@ -390,7 +390,7 @@ def list_resources_unique_id_from_manifest(manifest: Manifest):
 
     setattr(dbt_flags, "output", "json")
     setattr(dbt_flags, "output", "json")
-    setattr(dbt_flags, "output_keys", "unique_id,name")
+    setattr(dbt_flags, "output_keys", "unique_id,name,resource_type")
     setattr(dbt_flags, "selector_name", None)
     setattr(dbt_flags, "resource_types", [])
 
@@ -495,6 +495,11 @@ class ChangeSet:
         self.target: Dict = target
         self.base_manifest: Manifest = self._m(base)
         self.target_manifest: Manifest = self._m(target)
+
+        # resources in this format [{unique_id, name, resource_type}]
+        self.base_resources = list_resources_unique_id_from_manifest(self.base_manifest)
+        self.target_resources = list_resources_unique_id_from_manifest(self.target_manifest)
+
         self.explicit_changes = sorted(self._do_list_explicit_changes())
 
     def _m(self, run: Dict):
@@ -503,14 +508,20 @@ class ChangeSet:
             raise ValueError('Cannot find .dbt.manifest in run data')
         return load_manifest(manifest)
 
-    def _do_list_explicit_changes(self):
-        base_resources = [x.get('unique_id') for x in list_resources_unique_id_from_manifest(self.base_manifest)]
-        target_resources = [x.get('unique_id') for x in list_resources_unique_id_from_manifest(self.target_manifest)]
+    def resolve_unique_id(self, resource_name: str, resource_type: str):
+        for entry in self.base_resources + self.target_resources:
+            if entry.get('resource_type') == resource_type and entry.get('name') == resource_name:
+                return entry.get('unique_id')
 
-        # exclude added and removed
+    def _do_list_explicit_changes(self):
+        base_resources = [x.get('unique_id') for x in self.base_resources]
+        target_resources = [x.get('unique_id') for x in self.target_resources]
+
         resource_in_both = list(set(base_resources).intersection(target_resources))
+        # exclude added and removed by intersection with common resources
         output = [x.get('unique_id') for x in list_changes_in_unique_id(self.base_manifest, self.target_manifest, True)]
         output = list(set(output).intersection(resource_in_both))
+
         return output
 
     def list_explicit_changes(self):
@@ -527,6 +538,12 @@ class ChangeSet:
                 if metrics_b.get(x) != metrics_t.get(x):
                     ref_id = metrics_t.get(x).get('ref_id')
                     if ref_id:
+                        diffs.append(ref_id)
+                    else:
+                        # resolve the unique id for the legacy report
+                        metric_name = metrics_t.get(x).get('headers')[-1]
+                        ref_id = self.resolve_unique_id(metric_name, 'metric')
+                        assert ref_id is not None
                         diffs.append(ref_id)
 
         implicit = list(set(diffs))
@@ -547,7 +564,12 @@ class ChangeSet:
                 if r1 == r2 and c1 == c2 and not self.has_changed(b, t):
                     pass
                 else:
-                    diffs.append(ref_id)
+                    if ref_id:
+                        diffs.append(ref_id)
+                    else:
+                        resolved_id = self.resolve_unique_id(table_name, 'model')
+                        assert resolved_id is not None
+                        diffs.append(resolved_id)
 
         return diffs
 
