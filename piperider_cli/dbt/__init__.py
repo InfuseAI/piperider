e1bf110b954bf742566a38d1d0d3d8837e5c5532
Ching Yi, Chan <qrtt1@infuseai.io>
2023-07-12 10:20:37 +0800

    Disalbe dbt-logger
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- piperider_cli/dbt/__init__.py
+++ piperider_cli/dbt/__init__.py
@@ -10,4 +10,11 @@ def disable_dbt_compile_stats():
         def __exit__(self, exc_type, exc_val, exc_tb):
             compilation.print_compile_stats = self.original_print_compile_stats
 
+    try:
+        from dbt.events.functions import cleanup_event_logger
+        cleanup_event_logger()
+    except ImportError:
+        # we dont support the dbt version less than 1.4
+        pass
+
     return context_class()
